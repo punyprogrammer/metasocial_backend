bf289930b3dbc5522d7ed82351df7396c1464c73
Amardeep Ganguly <you@example.com>
2021-12-10 08:28:20 +0530

    second

--- app.js
+++ app.js
@@ -26,7 +26,10 @@ mongoose.connect(
 );
 //middleware
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://amar-blog.herokuapp.com"); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://amarsocial.herokuapp.com/"
+  ); // update to match the domain you will make the request from
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
