ba88b4fad5fdb75bf7542797a17ee5791361f8a1
Amardeep Ganguly <you@example.com>
2021-12-14 21:17:02 +0530

    fsdfsd

--- routes/posts.js
+++ routes/posts.js
@@ -74,7 +74,7 @@ router.put("/:id/comment", async (req, res) => {
   }
 });
 //get all comments of a post
-router.get("/:id/comment/", async (req, res) => {
+router.get("/:id/comment", async (req, res) => {
   try {
     const comments = await Post.findById(req.params.id).select("comments");
     res.status(500).json(comments);
